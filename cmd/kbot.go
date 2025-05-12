432ebf48d02c0ccc9dc6f038a3452655d6980189
serhii.raskolei <serhii.raskolei@gmail.com>
2025-05-12 08:33:56 +0300

    Refactor kbot command handlers to improve logging and error handling for quotes and weather commands

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,24 +1,29 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
 	telebot "gopkg.in/telebot.v4"
 )
 
-// var (
-// 	TelegramToken = os.Getenv("TelegramToken")
-// )
+func LogUserMessage(c telebot.Context) {
+	user := c.Sender()
+	text := c.Text()
+	log.Printf("User %s (%d) wrote: %s", user.Username, user.ID, text)
+}
 
 var kbotCmd = &cobra.Command{
 	Use:     "kbot",
 	Aliases: []string{"start"},
-	Short:   "Telegram bot for conversation",
-	Long:    `A simple telegram bot for conversation with a user.`,
+	Short:   "Telegram bot",
+	Long:    `A simple telegram bot`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		TelegramToken := os.Getenv("TelegramToken")
@@ -39,22 +44,75 @@ var kbotCmd = &cobra.Command{
 		}
 
 		kbot.Handle("/start", func(c telebot.Context) error {
-			// return c.Send("Hello! I'm a simple bot. How can I help you?")
-			user := c.Sender()
-			text := c.Text()
-			log.Printf("User %s (%d) wrote: %s", user.Username, user.ID, text)
-			return c.Send("Hello! I'm a simple bot. How can I help you?" + text)
+			LogUserMessage(c)
+			return c.Send("Hello! I'm a simple bot.\n\n1. /quote - get a random quote\n2. /weather <city> - get the weather for a city")
 		})
 
 		kbot.Handle("/quote", func(c telebot.Context) error {
-			quotes := []string{
-				"Не помиляється той, хто нічого не робить.",
-				"Великі справи починаються з маленьких кроків.",
-				"Успіх — це рух від невдачі до невдачі без втрати ентузіазму.",
-				"Життя — це 10% того, що з тобою відбувається, і 90% того, як ти на це реагуєш.",
+			LogUserMessage(c)
+			url := ("https://dummyjson.com/quotes/random")
+			resp, err := http.Get(url)
+			if err != nil {
+				return c.Send("Could not fetch quote: " + err.Error())
 			}
-			idn := time.Now().Unix() % int64(len(quotes))
-			return c.Send(quotes[idn])
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				return c.Send("Failed to get quote")
+			}
+			var data struct {
+				Quote  string `json:"quote"`
+				Author string `json:"author"`
+			}
+			if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+				return c.Send("Could not decode quote data: " + err.Error())
+			}
+			msg := fmt.Sprintf("Quote: %s\n\nAuthor: %s", data.Quote, data.Author)
+			return c.Send(msg)
+		})
+
+		kbot.Handle("/weather", func(c telebot.Context) error {
+			LogUserMessage(c)
+			args := c.Args()
+			if len(args) == 0 {
+				return c.Send("Please provide a city name. Example: /weather Kyiv")
+			}
+			city := strings.Join(args, " ")
+			apiKey := os.Getenv("WeatherApiKey")
+			if apiKey == "" {
+				return c.Send("WeatherApiKey environment variable is not set")
+			}
+			url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=yes", apiKey, city)
+			resp, err := http.Get(url)
+			if err != nil {
+				return c.Send("Could not fetch weather data: " + err.Error())
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != 200 {
+				return c.Send("Failed to get weather for this city:" + city)
+			}
+
+			var data struct {
+				Location struct {
+					Name      string `json:"name"`
+					Country   string `json:"country"`
+					Localtime string `json:"localtime"`
+				} `json:"location"`
+				Current struct {
+					TempC     float64 `json:"temp_c"`
+					Condition struct {
+						Text string `json:"text"`
+						Icon string `json:"icon"`
+					} `json:"condition"`
+				} `json:"current"`
+			}
+
+			if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+				return c.Send("Could not decode weather data: " + err.Error())
+			}
+
+			msg := fmt.Sprintf("Weather in %s, %s\nTemperature: %.1f°C - %s\nTime: %s\n%s\n", data.Location.Name, data.Location.Country, data.Current.TempC, data.Current.Condition.Text, data.Location.Localtime, data.Current.Condition.Icon)
+			return c.Send(msg)
 		})
 
 		kbot.Start()
